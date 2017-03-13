08db54eade2b8e78858846346d3425b18425e974
David Wen <dstwen@gmail.com>
2017-03-12 19:17:46 -0700

    Fix up devise account settings page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,15 +8,18 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) do |user_params|
-      user_params.permit(
-        :email,
-        :password,
-        :first_name,
-        :last_name,
-        :program,
-        :graduation_year
-      )
+    [:sign_up, :account_update].each do |action|
+      devise_parameter_sanitizer.for(action) do |user_params|
+        user_params.permit(
+          :email,
+          :password,
+          :password_confirmation,
+          :first_name,
+          :last_name,
+          :program,
+          :graduation_year
+        )
+      end
     end
   end
 end
