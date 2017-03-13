56c5e4525262d0348685bddedb960ed8d88454cb
David Wen <dstwen@gmail.com>
2017-03-12 19:23:42 -0700

    Send email after user creation

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,10 @@
 class RegistrationsController < Devise::RegistrationsController
 
+  def create
+    super
+    UserMailer.welcome_email(resource).deliver_now unless resource.invalid?
+  end
+
   protected
 
   def update_resource(resource, params)
