c4ca5f757d5431a96e2886abc026c3cf290dd6cd
David Wen <dstwen@gmail.com>
2017-03-12 19:04:04 -0700

    Fix up notices and alerts - especially in devise

---
+++ app/helpers/devise_helper.rb
@@ -0,0 +1,16 @@
+module DeviseHelper
+  def devise_error_messages!
+    return "" unless devise_error_messages?
+
+    messages = resource.errors.full_messages.to_sentence.downcase.capitalize
+    sentence = I18n.t("errors.messages.not_saved", :count => resource.errors.count, :resource => resource.class.model_name.human.downcase)
+
+    flash[:alert] = messages.html_safe
+    return ""
+  end
+
+  def devise_error_messages?
+    !resource.errors.empty?
+  end
+
+end
