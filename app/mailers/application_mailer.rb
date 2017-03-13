aaf7b864e9faff8ef0c7146f88e3aa4a0bf103ab
David Wen <dstwen@gmail.com>
2017-03-12 18:40:50 -0700

    Updates to welcome email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,3 +1,3 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "postmaster@sandbox3f41811b57b24d8dbef2692dd165935b.mailgun.org"
+  default from: '"Kellogg in a Bottle" <kellogginabottle@gmail.com>'
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
-    @url  = 'http://example.com/login'
+    @url  = Rails.application.routes.url_helpers.root_url(host: ENV["HOST"])
     mail(to: @user.email, subject: 'Welcome to My Awesome Site')
   end
 end
