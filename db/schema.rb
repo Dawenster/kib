758a9ab8b54a1ec58b6238e5fce525558e395c49
David Wen <dstwen@gmail.com>
2017-03-13 07:07:28 -0700

    Add course category id to courses

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170313135413) do
+ActiveRecord::Schema.define(version: 20170313140656) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,8 +27,9 @@ ActiveRecord::Schema.define(version: 20170313135413) do
     t.string   "code"
     t.string   "name"
     t.boolean  "active"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.integer  "course_category_id"
   end
 
   create_table "users", force: :cascade do |t|
