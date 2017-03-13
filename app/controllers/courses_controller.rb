d402dd396158c884edc39510b631cf275d8354e6
David Wen <dstwen@gmail.com>
2017-03-13 08:00:02 -0700

    Display courses

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,15 @@
+class CoursesController < ApplicationController
+  def index
+    if params[:s].present?
+      search_term = "%#{params[:s].downcase}%"
+      query = "LOWER(code) LIKE ? OR LOWER(name) LIKE ?"
+      courses = Course.active.where(query, search_term, search_term)
+      @course_categories = CourseCategory.where(id: courses.pluck(:course_category_id)).order(:name)
+      @course_ids = courses.pluck(:id)
+      @searched_term = params[:s]
+    else
+      @course_categories = CourseCategory.order(:name).joins(:courses).group("course_categories.id")
+      @course_ids = Course.pluck(:id)
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
   root "pages#landing"
+
+  resources :courses, only: [:index]
 end
