0ba1003fed37453d4af9d71e37f34b8d84a96a4d
David Wen <dstwen@gmail.com>
2017-03-13 18:58:43 -0700

    Fail nicely on creating users

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,6 +3,8 @@ class RegistrationsController < Devise::RegistrationsController
   def create
     super
     UserMailer.welcome_email(resource).deliver_now unless resource.invalid?
+  rescue => e
+    Rollbar.error(e, user_email: resource.email)
   end
 
   protected
