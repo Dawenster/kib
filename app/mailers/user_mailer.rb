be34ace1fbf3e499c7ceb7bec35265c449d40f0a
David Wen <dstwen@gmail.com>
2017-03-14 08:32:38 -0700

    Change mailer call to action url

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
-    @url  = Rails.application.routes.url_helpers.root_url(host: ENV["HOST"])
+    @url  = Rails.application.routes.url_helpers.courses_url(host: ENV["HOST"])
     mail(to: @user.email, subject: 'Welcome to My Awesome Site')
   end
 end
