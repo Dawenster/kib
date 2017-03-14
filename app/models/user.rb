edfabd4521b2438c6082c35d295c59e4a1f12200
David Wen <dstwen@gmail.com>
2017-03-13 20:34:34 -0700

    Add more form inputs and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,14 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :first_name, :last_name, presence: true
+  VALID_PROGRAMS = ["1Y", "2Y", "MMM", "JD MBA", "Part-time", "EMBA", "Other"]
+  VALID_YEAR_START = Time.current.year - 1
+  VALID_YEAR_END = VALID_YEAR_START + 9
+  VALID_YEARS = (VALID_YEAR_START..VALID_YEAR_END).to_a.map(&:to_s)
+
+  validates :first_name, :last_name, :program, :graduation_year, presence: true
+  validates_inclusion_of :program, :in => VALID_PROGRAMS, message: "must be selected"
+  validates_inclusion_of :graduation_year, :in => VALID_YEARS, message: "must be between #{VALID_YEAR_START} and #{VALID_YEAR_END}"
 
   def full_name
     "#{first_name} #{last_name}"
