7c3fe81663743356436d4e2345a2f6bacf4ae9fd
David Wen <dstwen@gmail.com>
2017-03-26 08:38:59 -0700

    Store current location and reroute after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,19 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :store_current_location, unless: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+    stored_location = stored_location_for(:user)
+    stored_location = root_path if stored_location.blank? || stored_location == "/"
+    stored_location
+  end
 
   protected
+
+  def store_current_location
+    store_location_for(:user, request.url) if request.get?
+  end
 
   def configure_permitted_parameters
     [:sign_up, :account_update].each do |action|
