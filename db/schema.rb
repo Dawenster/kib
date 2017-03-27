126b1bfed3e8c4095c5954b98cdaeffa07ac5591
David Wen <dstwen@gmail.com>
2017-03-26 17:16:23 -0700

    Add class ID to requests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170327001347) do
+ActiveRecord::Schema.define(version: 20170327001557) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20170327001347) do
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
     t.boolean  "assigned",   default: false
+    t.integer  "class_id"
   end
 
   create_table "users", force: :cascade do |t|
