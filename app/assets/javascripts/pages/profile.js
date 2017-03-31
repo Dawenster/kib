0453be3bdd173522621a9463c6c73444045baaa7
David Wen <dstwen@gmail.com>
2017-03-30 23:25:23 -0700

    Front-end for assignable button

--- app/assets/javascripts/pages/profile.js
+++ app/assets/javascripts/pages/profile.js
@@ -7,4 +7,13 @@ $(document).ready(function(){
     ordering: true,
     order: [[ 1, 'asc' ]]
   });
+
+  var assignableSwitch = document.querySelector(".assignable-switch");
+  if (assignableSwitch) {
+    var init = new Switchery(assignableSwitch, {color: "#28a5d4"});
+    assignableSwitch.onchange = function() {
+      console.log(assignableSwitch.checked)
+    };
+  }
+
 })
