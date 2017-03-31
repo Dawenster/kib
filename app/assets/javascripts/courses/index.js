03cc7909c93c6ca8e6df53e375f54ef63cfd0a36
David Wen <dstwen@gmail.com>
2017-03-30 22:52:46 -0700

    Remove sweet alert confirm on requesting

--- app/assets/javascripts/courses/index.js
+++ app/assets/javascripts/courses/index.js
@@ -1,28 +1,28 @@
 $(document).ready(function(){
-  $("body").on("click", ".request-btn", function(e) {
-    e.preventDefault();
-    var url = $(this).data("url");
-    var requestRole = $(this).data("request-role");
-    var requestCourse = $(this).data("request-course");
-    var descriptionText = "You are committing to be a " + requestRole + " for the course:" + "\n" + requestCourse
+  // $("body").on("click", ".request-btn", function(e) {
+  //   e.preventDefault();
+  //   var url = $(this).data("url");
+  //   var requestRole = $(this).data("request-role");
+  //   var requestCourse = $(this).data("request-course");
+  //   var descriptionText = "You are committing to be a " + requestRole + " for the course:" + "\n" + requestCourse
 
-    swal({
-      title: "Are you sure?",
-      text: descriptionText,
-      type: "warning",
-      showCancelButton: true,
-      confirmButtonText: "Yes!",
-      closeOnConfirm: false,
-      showLoaderOnConfirm: true
-    },
-    function(){
-      $("button.confirm").attr("disabled", "disabled")
-      $.ajax({
-        url: url,
-        method: "post"
-      }).done(function(data) {
-        window.location.reload(false);
-      })
-    });
-  })
+  //   swal({
+  //     title: "Are you sure?",
+  //     text: descriptionText,
+  //     type: "warning",
+  //     showCancelButton: true,
+  //     confirmButtonText: "Yes!",
+  //     closeOnConfirm: false,
+  //     showLoaderOnConfirm: true
+  //   },
+  //   function(){
+  //     $("button.confirm").attr("disabled", "disabled")
+  //     $.ajax({
+  //       url: url,
+  //       method: "post"
+  //     }).done(function(data) {
+  //       window.location.reload(false);
+  //     })
+  //   });
+  // })
 })
