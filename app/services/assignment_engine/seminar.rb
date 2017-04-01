dd452d82c64a62fa8d41765dc5d319d346621dc3
David Wen <dstwen@gmail.com>
2017-04-01 11:34:21 -0700

    Setup AE Seminar specs

--- app/services/assignment_engine/seminar.rb
+++ app/services/assignment_engine/seminar.rb
@@ -1,11 +1,13 @@
 class AssignmentEngine::Seminar
 
+  attr_reader :course
+
   def initialize(course_id)
     @course = Course.find(course_id)
   end
 
   def create
-    
+
   end
 
 end
---
+++ spec/services/assignment_engine/seminar_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe AssignmentEngine::Seminar do
+
+  let(:course) { create(:course) }
+
+  context "#initialize" do
+
+    it "finds course" do
+      @ae_seminar = AssignmentEngine::Seminar.new(course.id)
+      expect(@ae_seminar.course).to eq(course)
+    end
+
+  end
+
+end
