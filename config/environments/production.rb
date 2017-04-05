6b85c45c88a3f0c9ba4ce8ba101f75cc65b481e0
David Wen <dstwen@gmail.com>
2017-04-04 21:09:14 -0700

    Switch to Sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,11 +81,12 @@ Rails.application.configure do
   # Mailgun setup
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :authentication => :plain,
-    :address => "smtp.mailgun.org",
+    :user_name => ENV["SENDGRID_USERNAME"],
+    :password => ENV["SENDGRID_PASSWORD"],
+    :domain => 'kellogginabottle.com',
+    :address => 'smtp.sendgrid.net',
     :port => 587,
-    :domain => ENV["MAILGUN_DOMAIN"],
-    :user_name => ENV["MAILGUN_USER_NAME"],
-    :password => ENV["MAILGUN_PASSWORD"]
+    :authentication => :plain,
+    :enable_starttls_auto => true
   }
 end
