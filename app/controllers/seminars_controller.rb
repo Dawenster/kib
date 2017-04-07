c4216023c6fbc75c4f7aaee099aa98fcb6b49f9c
David Wen <dstwen@gmail.com>
2017-04-06 22:14:52 -0700

    Only show finalized classes

--- app/controllers/seminars_controller.rb
+++ app/controllers/seminars_controller.rb
@@ -4,8 +4,8 @@ class SeminarsController < ApplicationController
 
   def index
     @user = current_user
-    @seminars_as_student = @user.assigned_seminars_as_student.order(:scheduled_at)
-    @seminars_as_teacher = @user.assigned_seminars_as_teacher.order(:scheduled_at)
+    @seminars_as_student = @user.assigned_seminars_as_student.finalized.order(:scheduled_at)
+    @seminars_as_teacher = @user.assigned_seminars_as_teacher.finalized.order(:scheduled_at)
   end
 
   def edit
