1896dadde3db6a45f8e3df9cc3ea2ecafd754e9e
David Wen <dstwen@gmail.com>
2017-04-12 21:37:38 -0700

    Update email content

--- app/mailers/seminar_mailer.rb
+++ app/mailers/seminar_mailer.rb
@@ -12,7 +12,7 @@ class SeminarMailer < ApplicationMailer
   def seminar_assigned_as_teacher(user, seminar)
     @user = user
     @course = seminar.course
-    @students_as_string = seminar.students.map{|student| "#{student.full_name} (#{student.email})"}.to_sentence
+    @students_as_string = seminar.students.map{|student| "<div>#{student.full_name} (#{student.email})</div>"}.join("")
     @url  = Rails.application.routes.url_helpers.classes_url(host: ENV["HOST"])
     email = email_to_use(@user)
     mail(to: email, subject: "#{@course.code}: Teacher Request Confirmed")
