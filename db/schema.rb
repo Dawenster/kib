4c87e38abd41aa447e950c21f150a458d7d838d8
David Wen <dstwen@gmail.com>
2017-04-13 20:50:28 -0700

    Add teacher_id to reviews

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170412152137) do
+ActiveRecord::Schema.define(version: 20170414034651) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20170412152137) do
     t.text     "feedback_for_kib"
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
+    t.integer  "teacher_id"
   end
 
   create_table "seminars", force: :cascade do |t|
